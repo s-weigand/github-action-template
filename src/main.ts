c049547ed21cde22f477f398b48a56519cd70b5f
s-weigand <s.weigand.phy@gmail.com>
2019-11-22 19:25:45 +0100

    made tests pass

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-    const branch = core.getInput('dummy_input')
+    const dummy_input = core.getInput('dummy_input')
   } catch (error) {
     core.setFailed(error.message)
   }
